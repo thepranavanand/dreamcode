1966a171521454fb7a5f5d796d56392f59f5b0ee
Pranav Anand <thepranavanand@gmail.com>
2022-02-13 17:04:37 +0530

    Made some changes

--- A LAB/LAB/a.cpp
+++ A LAB/LAB/a.cpp
@@ -1,37 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
 
-#include "bits/stdc++.h"
-using namespace std;
-
-
- // } Driver Code Ends
-// User function Template for C++
-
-class Solution {
-  public:
-    int minimumAttacks(int X, int K) 
+int find_nth_term(int n, int a, int b, int c) {
+    int i, j, nth_term;
+    for(i = 0; i<n-3; i++)
     {
-        int T = 1;
-        int i = 2;
-        while(X<=K)
-        {
-            X+=i;
-            i = i+2;
-            T +=1;
-        }
-        return T;
+        int str[3] = {a, b, c};
+        nth_term = str[0]+str[1]+str[2];
+            for(j = 0; j < 2; j++)
+            {
+                str[j] = str[j+1];
+            }
+        str[2] = nth_term;
     }
-};
-
-
-// { Driver Code Starts.
+    return nth_term;
+}
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int X, K;
-        cin >> X >> K;
-        Solution ob;
-        cout << ob.minimumAttacks(X, K) << "\n";
-    }
-}  // } Driver Code Ends
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
