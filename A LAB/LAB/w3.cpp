c1faad48e42832ecffdabad8898a2ded7a49f5b4
Pranav Anand <thepranavanand@gmail.com>
2022-02-14 22:20:08 +0530

    Added a cpp program with cin.get()

--- A LAB/LAB/w3.cpp
+++ A LAB/LAB/w3.cpp
@@ -10,3 +10,17 @@ int main()
 
     return 0;
 }
+
+// C++ program to demonstrate cin.get()
+  
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    char name[25];
+    cin.get(name, 25);
+    cout << name;
+  
+    return 0;
+}
