81f382dbad94e7386bf7e9c4db28d2f0442161e9
Pranav Anand <thepranavanand@gmail.com>
2022-04-14 18:22:47 +0530

    Some info about header file in the cherno

---
+++ C++/TheCherno/05_header.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"forest.h"
+// void LOG(const char* word)
+// {
+//     std::cout<<word<<std::endl;
+// }
+int main ()
+{
+    LOG("Hello World");
+    // std::cin.get();
+}
